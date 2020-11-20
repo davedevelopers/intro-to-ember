f0399b193f130a38649110865fb573a3db700f43
davedeveloper <ddave2001@gmail.com>
2020-11-20 16:35:17 +0530

    Built-in helpers, each, if,

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -1,15 +1,16 @@
 import Controller from '@ember/controller';
-import {tracked} from '@glimmer/tracking';
-import {action} from '@ember/object'
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
 
 export default class ItemController extends Controller {
-    @tracked color='red';
+  @tracked color = this.model.colors[0].color;
 
-    get productImage(){
-        return `/assets/images/beats-solo-${this.color}.png`
-    }
-    @action
-    onChangeColor(newColor){
-        this.color=newColor;
-    }
+  get productImage() {
+    return this.model.colors.find(({ color }) => color === this.color).image;
+  }
+
+  @action
+  onChangeColor(newColor) {
+    this.color = newColor;
+  }
 }
--- app/routes/item.js
+++ app/routes/item.js
@@ -1,10 +1,13 @@
 import Route from '@ember/routing/route';
+import { products } from '../data/products';
 
 export default class ItemRoute extends Route {
-    model(params){
-        const {
-            item_id
-        } = params;
-        return item_id;
-    }
+  model(params) {
+    const {
+      item_id
+    } = params;
+    const product = products.find((product) => product.id === item_id);
+    console.log(product)
+    return product;
+  }
 }
